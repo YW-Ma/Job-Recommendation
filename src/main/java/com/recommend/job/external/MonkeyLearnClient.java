cbec65e541f8a7cb90c087ec21cfe272018b985a
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-11-08 20:58:41 +0800

    a main function for test propuse.

--- src/main/java/com/recommend/job/external/MonkeyLearnClient.java
+++ src/main/java/com/recommend/job/external/MonkeyLearnClient.java
@@ -104,4 +104,14 @@ public class MonkeyLearnClient {
             return Collections.emptyList();
         }
     }
+
+    // a main function for test
+    public static void main(String[] args) {
+        List<String> articles = Arrays.asList("Elon Musk has shared a photo of the spacesuit designed by SpaceX. This is the second image shared of the new design and the first to feature the spacesuit’s full-body look.",
+                "Former Auburn University football coach Tommy Tuberville defeated ex-US Attorney General Jeff Sessions in Tuesday nights runoff for the Republican nomination for the U.S. Senate. ",
+                "The NEOWISE comet has been delighting skygazers around the world this month – with photographers turning their lenses upward and capturing it above landmarks across the Northern Hemisphere.");
+        MonkeyLearnClient client = new MonkeyLearnClient();
+        List<Set<String>> keywordList = client.extract(articles);
+        System.out.println(keywordList);
+    }
 }
